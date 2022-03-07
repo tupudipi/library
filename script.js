6dc66b1d1ec31edd85d313f6af81e60a480a14cb
tupudipi <tupudipi@gmail.com>
2022-03-07 10:05:06 +0200

    warmup

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let library = document.getElementById("library");
 
 let libraryArray = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.addToLibrary = function () {
-    libraryArray.push(this);
-}
-
-Book.prototype.changeRead = function() {
-    this.read = !this.read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    addToLibrary() {
+        libraryArray.push(this);
+    }
+    changeRead() {
+        this.read = !this.read;
+    }
 }
 
 let book1 = new Book('The Hobbit', 'JRR Tolkien', 310, false);
@@ -34,18 +34,22 @@ function displayAllBooks() {
         bookToBeAdded.id = 'book';
         bookToBeAdded.className = 'book';
         bookToBeAdded.dataset.index = index;
+
         let title = document.createElement('p')
             title.id = 'title';
             title.textContent = book.title;
             bookToBeAdded.appendChild(title);
+
         let author = document.createElement('p')
             author.id = 'author';
             author.textContent = 'by ' + book.author;
             bookToBeAdded.appendChild(author);
+
         let pages = document.createElement('p')
             pages.id = 'pages';
             pages.textContent = book.pages + ' pages long';
             bookToBeAdded.appendChild(pages);
+
         let read = document.createElement('p')
             read.id = 'read';
             if (book.read)
